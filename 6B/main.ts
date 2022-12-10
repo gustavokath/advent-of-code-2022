397dce8f449386612d03d7caaad2ef49f2a54e46
Gustavo Kath <gustavo.kath@sap.com>
2022-12-10 20:32:00 -0300

    Day 6

---
+++ 6B/main.ts
@@ -0,0 +1,36 @@
+import { lineUtil } from "../util.js";
+
+const SIZE_OF_MARKER = 14;
+
+const findInitialPackage = (buffer: string): number => {
+  const letters = new Map<string, number>
+  for(let i=0; i < buffer.length; i++){
+    const outOfWindowIndex = i - SIZE_OF_MARKER;
+    const current = buffer[i];
+
+    if(outOfWindowIndex >= 0){
+      const outOfWindow = buffer[outOfWindowIndex];
+
+      if(letters.get(outOfWindow)! > 1) {
+        letters.set(outOfWindow, letters.get(outOfWindow)! - 1);
+      } else {
+        letters.delete(outOfWindow)
+      }
+    }
+
+    if(letters.has(current)) {
+      letters.set(current, letters.get(current)! + 1);
+    } else {
+      letters.set(current, 1);
+    }
+
+    if(letters.size === SIZE_OF_MARKER) {
+      return i+1;
+    }
+  }
+  return -1;
+}
+
+for await (const line of lineUtil) {
+  console.log(findInitialPackage(line));
+}
